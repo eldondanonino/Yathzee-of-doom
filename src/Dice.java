cd8ffc85d8022e15d8705fa1d3fe49d566bfe052
Unknown <daniil.rosso@efrei.net>
2019-09-02 09:17:34 -0500

    documentation

--- src/Dice.java
+++ src/Dice.java
@@ -25,10 +25,10 @@ public class Dice
                 System.out.println(" ");
         }
         if (toggle)
-            System.out.println("Your sum of dice has a value of  " + sum + " points!\n");
+            System.out.println("Your sum of dice has a value of  " + sum + " points!\n"); // if true, we will output the sum of our hand (doesnt work if we have some -1 values) 
     }
 
-    public static int Reroll (int[] ArrayOfDice, int[] Failsafe, int turnCounter)
+    public static int Reroll (int[] ArrayOfDice, int[] Failsafe, int turnCounter) //method that rerolls the value of selected dice
     {
         int n, input = 0;
         Scanner sc = new Scanner(System.in);
